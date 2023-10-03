555b64e96dfbf85d365b27625d21d26c09159436
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-03 18:22:33 +0200

    add: UserController

---
+++ user-service/src/main/java/com/tpa/userservice/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.tpa.userservice.controller;
+
+import com.tpa.userservice.dto.SignUpRequest;
+import com.tpa.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody SignUpRequest request){
+        userService.createUser(request);
+    }
+}
