cd52f695f31a090014096fe2e2aab47106d76624
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-14 23:13:53 +0200

    add: model classes

---
+++ game-service/src/main/java/com/tpa/gameservice/model/Game.java
@@ -0,0 +1,22 @@
+package com.tpa.gameservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(value = "game")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Game {
+    @Id
+    private String id;
+    private List<Move> moves;
+    private Player[] players;
+}
