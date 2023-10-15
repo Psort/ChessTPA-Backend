b0fd17d0925862e538e4773d5ff9847f5c4f97c7
Psort <skowronpiotr.00@gmail.com>
2023-10-15 21:55:53 +0200

    refactor: user unique email

--- user-service/src/main/java/com/tpa/userservice/model/User.java
+++ user-service/src/main/java/com/tpa/userservice/model/User.java
@@ -1,9 +1,6 @@
 package com.tpa.userservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,5 +16,7 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String email;
 }
