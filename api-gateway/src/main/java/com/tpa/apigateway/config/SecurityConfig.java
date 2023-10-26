c66aa6a6d52f8b341046cbf031a4dfda1b31636c
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-10-26 17:07:56 +0200

    add: Cors config

--- api-gateway/src/main/java/com/tpa/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/tpa/apigateway/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.tpa.apigateway.config;
 
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,13 +20,22 @@ public class SecurityConfig {
     SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeExchange((authorize) -> authorize
                         .pathMatchers("/eureka/**","/api/auth/**")
                         .permitAll()
                         .anyExchange().authenticated()
                 )
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(GlobalCorsProperties globalCorsProperties) {
+        var source = new UrlBasedCorsConfigurationSource();
+        globalCorsProperties.getCorsConfigurations().forEach(source::registerCorsConfiguration);
+        return source;
     }
 }
