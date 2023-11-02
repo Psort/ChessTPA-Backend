4bfe17e91b65d2483f9461a707a4e8bd4712fbba
Psort <skowronpiotr.00@gmail.com>
2023-11-02 16:35:25 +0100

    add: game-service: castleType

---
+++ game-service/src/main/java/com/tpa/gameservice/model/castleType.java
@@ -0,0 +1,13 @@
+package com.tpa.gameservice.model;
+
+public enum castleType {
+    SHORTWHITE("K"),
+    LONGWHITE("Q"),
+    SHORTBLACK("k"),
+    LONGBLACK("q"),
+    ;
+
+    castleType(String value) {
+
+    }
+}
