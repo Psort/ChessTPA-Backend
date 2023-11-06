656183a4009559ee4a0e2f0e5fc8f188853310c4
Psort <skowronpiotr.00@gmail.com>
2023-11-06 21:25:38 +0100

    clean Code

--- accesscontrol-service/src/main/java/org/tpa/useraccessservice/config/KeycloakProvider.java
+++ accesscontrol-service/src/main/java/org/tpa/useraccessservice/config/KeycloakProvider.java
@@ -1,4 +1,6 @@
 package org.tpa.useraccessservice.config;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.KeycloakBuilder;
@@ -26,15 +28,15 @@ public class KeycloakProvider {
                     .build();
     }
 
-    public KeycloakBuilder newKeycloakBuilderWithPasswordCredentials(String username, String password) {
+    public Keycloak newKeycloakBuilderWithPasswordCredentials(String username, String password) {
         return KeycloakBuilder.builder() //
                 .realm(realm) //
                 .serverUrl(serverUrl)//
                 .clientId(clientID) //
                 .clientSecret(clientSecret) //
-                .grantType("password")
+                .grantType(OAuth2Constants.PASSWORD)
                 .username(username) //
-                .password(password);
+                .password(password)
+                .build();
     }
-
 }
