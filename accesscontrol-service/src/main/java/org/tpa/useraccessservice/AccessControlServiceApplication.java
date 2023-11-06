6195457154335f675bf5bcdee7d89346640f802f
Psort <skowronpiotr.00@gmail.com>
2023-11-06 21:28:29 +0100

    add EnableDiscoveryClient in AccessControlServiceApplication

--- accesscontrol-service/src/main/java/org/tpa/useraccessservice/AccessControlServiceApplication.java
+++ accesscontrol-service/src/main/java/org/tpa/useraccessservice/AccessControlServiceApplication.java
@@ -2,8 +2,10 @@ package org.tpa.useraccessservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccessControlServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccessControlServiceApplication.class, args);
