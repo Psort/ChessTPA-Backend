fcd97137f3b2483e68955efb613b02b603c3e43e
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-14 16:12:07 +0100

    add user-service: addGame method

--- user-service/src/main/java/com/tpa/userservice/controller/UserController.java
+++ user-service/src/main/java/com/tpa/userservice/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.tpa.userservice.controller;
 
+import com.tpa.userservice.dto.NewGameRequest;
 import com.tpa.userservice.dto.SignUpRequest;
 
 import com.tpa.userservice.dto.UserResponse;
-import com.tpa.userservice.model.User;
 import com.tpa.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -25,5 +26,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<UserResponse> getUsernameByEmail(@RequestParam String email){
         return userService.getUserByEmail(email);
+    }
+
+    @PostMapping("/game")
+    @ResponseStatus(HttpStatus.OK)
+    public void addGame(@RequestBody NewGameRequest request){
+        userService.addGame(request);
     }
 }
