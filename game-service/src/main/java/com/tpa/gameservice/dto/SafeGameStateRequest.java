da994035ac18cb1a52fd925c29d1356d6c0acfda
Psort <skowronpiotr.00@gmail.com>
2023-11-16 15:15:22 +0100

    Add game-service: SafeGameStateRequest

---
+++ game-service/src/main/java/com/tpa/gameservice/dto/SafeGameStateRequest.java
@@ -0,0 +1,15 @@
+package com.tpa.gameservice.dto;
+
+
+import com.tpa.gameservice.model.GameStatus;
+import com.tpa.gameservice.model.Move;
+import lombok.Data;
+
+@Data
+
+public class SafeGameStateRequest {
+    String gameId;
+    String gameStatus;
+    Move move;
+    String boardState;
+}
---
+++ game-service/src/main/java/com/tpa/gameservice/model/PlayerColor.java
@@ -0,0 +1,10 @@
+package com.tpa.gameservice.model;
+
+public enum PlayerColor {
+    WHITE("white"),
+    BLACK("black");
+
+    PlayerColor(String color) {
+
+    }
+}
