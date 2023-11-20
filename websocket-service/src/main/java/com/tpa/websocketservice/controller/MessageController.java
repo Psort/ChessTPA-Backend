0cc5babb8d6254135097ceaa0b1e53b0d4613a15
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2023-11-20 17:21:24 +0100

    change websocket-service: change message destination to topic with gameId

--- websocket-service/src/main/java/com/tpa/websocketservice/controller/MessageController.java
+++ websocket-service/src/main/java/com/tpa/websocketservice/controller/MessageController.java
@@ -2,8 +2,10 @@ package com.tpa.websocketservice.controller;
 
 import com.tpa.websocketservice.service.WebSocketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,9 +13,9 @@ import org.springframework.stereotype.Controller;
 public class MessageController {
 
     private final WebSocketService webSocketService;
-    @MessageMapping("/game")
-    @SendTo("/topic/messages")
-    public String getMessages(String gameId){
+    @MessageMapping("/game/{gameId}")
+    @SendTo("/topic/messages/{gameId}")
+    public String getMessages(@DestinationVariable String gameId) {
         return webSocketService.getGame(gameId);
     }
 }
