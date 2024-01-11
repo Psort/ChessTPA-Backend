ec64f43b8602bc8053b90e0f99b81c882b63a92b
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-11 17:34:50 +0100

    change accesscontrol-service: add timestamp field at LogEvent class

--- accesscontrol-service/src/main/java/com/tpa/useraccessservice/event/LogEvent.java
+++ accesscontrol-service/src/main/java/com/tpa/useraccessservice/event/LogEvent.java
@@ -4,10 +4,14 @@ import com.tpa.useraccessservice.type.LogType;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 @Getter
 @Builder
 public class LogEvent {
-    private String serviceName;
-    private LogType type;
-    private String message;
+    private final String serviceName;
+    private final LogType type;
+    private final String message;
+    private final ZonedDateTime timestamp;
 }
