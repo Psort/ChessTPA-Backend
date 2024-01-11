300f095666dd3caa10c814dd40a44a4208cd4fc9
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-11 17:37:06 +0100

    change accesscontrol-service: add custom exception handling in WebClientService

--- accesscontrol-service/src/main/java/com/tpa/useraccessservice/service/WebClientService.java
+++ accesscontrol-service/src/main/java/com/tpa/useraccessservice/service/WebClientService.java
@@ -1,12 +1,16 @@
 package com.tpa.useraccessservice.service;
 
 import com.tpa.useraccessservice.dto.SignUpRequest;
+import com.tpa.useraccessservice.dto.UserResponse;
+import com.tpa.useraccessservice.exception.AccessRequestException;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Service
@@ -14,11 +18,14 @@ public class WebClientService {
     private final WebClient.Builder webClientBuilderWithLB;
     private final WebClient.Builder webClientBuilder;
 
-    public void sendUserToAddInUserService(SignUpRequest request) {
-        webClientBuilderWithLB.build().post().uri("http://user-service/api/user")
+    public void sendToUserService(SignUpRequest request) {
+        webClientBuilderWithLB.build()
+                .post()
+                .uri("http://user-service/api/user")
                 .bodyValue(request)
                 .retrieve()
-                .toBodilessEntity()
+                .bodyToMono(UserResponse.class)
+                .onErrorMap(RuntimeException.class, ex -> new AccessRequestException("User already exist"))
                 .block();
     }
 
