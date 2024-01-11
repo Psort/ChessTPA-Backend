fbf4a4f6e5d2b38455027caf19c7f9861f6bf715
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-11 17:35:23 +0100

    change accesscontrol-service: add timestamp field at LogEvent class

--- accesscontrol-service/src/main/java/com/tpa/useraccessservice/service/LogService.java
+++ accesscontrol-service/src/main/java/com/tpa/useraccessservice/service/LogService.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @RequiredArgsConstructor
 @Service
 public class LogService {
@@ -17,7 +20,9 @@ public class LogService {
                 "logManagementTopic",
                 LogEvent.builder()
                 .serviceName(serviceName)
-                .type(type).message(message)
+                .type(type)
+                .message(message)
+                .timestamp(ZonedDateTime.now(ZoneId.of("Europe/Warsaw")))
                 .build() );
 
     }
