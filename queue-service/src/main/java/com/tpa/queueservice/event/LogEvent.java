70df7c6edafaacbd5de1901f9981b5ce726dbd6f
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-15 16:59:44 +0100

    add queue-service: add timestamp to LogService & LogEvent

--- queue-service/src/main/java/com/tpa/queueservice/event/LogEvent.java
+++ queue-service/src/main/java/com/tpa/queueservice/event/LogEvent.java
@@ -4,10 +4,13 @@ package com.tpa.queueservice.event;
 import com.tpa.queueservice.type.LogType;
 import lombok.*;
 
+import java.time.ZonedDateTime;
+
 @Getter
 @Builder
 public class LogEvent {
     private String serviceName;
     private LogType type;
     private String message;
+    private ZonedDateTime timestamp;
 }
