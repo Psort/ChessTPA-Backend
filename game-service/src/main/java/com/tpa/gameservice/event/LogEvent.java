1c38e358442a98d6ed8bd76c28f2aa7330b25fd2
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-15 16:58:19 +0100

    add game-service: add timestamp to LogService & LogEvent

--- game-service/src/main/java/com/tpa/gameservice/event/LogEvent.java
+++ game-service/src/main/java/com/tpa/gameservice/event/LogEvent.java
@@ -5,10 +5,13 @@ import com.tpa.gameservice.type.LogType;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.ZonedDateTime;
+
 @Getter
 @Builder
 public class LogEvent {
     private String serviceName;
     private LogType type;
     private String message;
+    private ZonedDateTime timestamp;
 }
--- game-service/src/main/java/com/tpa/gameservice/service/LogService.java
+++ game-service/src/main/java/com/tpa/gameservice/service/LogService.java
@@ -7,18 +7,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @RequiredArgsConstructor
 @Service
 public class LogService {
     private final KafkaTemplate<String, LogEvent> kafkaTemplate;
 
     public void send(LogType type, String message) {
-        String serviceName = "AccessControl";
+        String serviceName = "GameService";
         kafkaTemplate.send(
                 "logManagementTopic",
                 LogEvent.builder()
                 .serviceName(serviceName)
                 .type(type).message(message)
+                .timestamp(ZonedDateTime.now(ZoneId.of("Europe/Warsaw")))
                 .build() );
 
     }
