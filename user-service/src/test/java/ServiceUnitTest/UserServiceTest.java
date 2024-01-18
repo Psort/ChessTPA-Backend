187a7ba8cc25a7cd2ab7cce3200412569135f76e
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-18 20:03:29 +0100

    change user-service: refactored & improved UserServiceTest class

--- user-service/src/test/java/ServiceUnitTest/UserServiceTest.java
+++ user-service/src/test/java/ServiceUnitTest/UserServiceTest.java
@@ -9,6 +9,8 @@ import com.tpa.userservice.repostiory.UserRepository;
 import com.tpa.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,7 +23,9 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -32,34 +36,31 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
+    private final String email = "test@mail.com";
+    private final String username = "test";
     @Test
     void shouldCreateUserWithProperValues() {
         // given
-        String email = "test@mail.com";
-        String username = "test";
+        SignUpRequest request = initializeSignupRequest();
 
-        SignUpRequest request = new SignUpRequest();
-        request.setEmail(email);
-        request.setUsername(username);
+        UserResponse expectedDTO = UserResponse.builder()
+                .username(username)
+                .email(email)
+                .build();
 
         UserResponse user = this.userService.createUser(request);
 
         // then
-        assertThat(user)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("email", email)
-                .hasFieldOrPropertyWithValue("username", username);
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).save(userArgumentCaptor.capture());
+        assertEquals(expectedDTO, user);
     }
 
     @Test
     void shouldNotCreateUserAndThrowUserRequestException() {
         // given
-        String email = "test@mail.com";
-        String username = "test";
-
-        SignUpRequest request = new SignUpRequest();
-        request.setEmail(email);
-        request.setUsername(username);
+        SignUpRequest request = initializeSignupRequest();
 
         User mockUser = User.builder()
                 .email(email)
@@ -67,7 +68,6 @@ public class UserServiceTest {
 
         // when
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(mockUser));
-
 
         // then
         assertThatThrownBy(() -> this.userService.createUser(request))
@@ -78,9 +78,6 @@ public class UserServiceTest {
     @Test
     void shouldGetUserByEmailAndReturnUserResponse() {
         // given
-        String email = "test@mail.com";
-        String username = "test";
-
         User mockUser = User.builder()
                 .email(email)
                 .username(username)
@@ -89,20 +86,18 @@ public class UserServiceTest {
         // when
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(mockUser));
 
+        UserResponse expectedDTO = UserResponse.builder()
+                .username(username)
+                .build();
+
         UserResponse result = this.userService.getUserByEmail(email);
 
         // then
-        assertThat(result)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("username", username);
+        assertEquals(expectedDTO, result);
     }
 
     @Test
     void shouldNotGetUserAndThrowUserRequestException() {
-        // given
-        String email = "test@mail.com";
-
-        // then
         assertThatThrownBy(() -> this.userService.getUserByEmail(email))
                 .isInstanceOf(UserRequestException.class)
                 .hasMessage("User does not exists");
@@ -136,6 +131,8 @@ public class UserServiceTest {
         this.userService.addGame(request);
 
         // then
+        verify(userRepository).saveAll(any(List.class));
+
         assertThat(firstPlayer)
                 .isNotNull()
                 .hasFieldOrPropertyWithValue("gameHistory", List.of(gameId));
@@ -162,35 +159,74 @@ public class UserServiceTest {
     }
 
     @Test
-    void shouldTradeEloPointsBetweenPlayers() {
+    void shouldAddEloPointsToWinningPlayer() {
         // given
+        User winningPlayer = initializeWinningPlayer();
+
+        User losingPlayer = initializeLosingPlayer();
+
+        // when
+        when(userRepository.findByUsername(winningPlayer.getUsername())).thenReturn(Optional.of(winningPlayer));
+        when(userRepository.findByUsername(losingPlayer.getUsername())).thenReturn(Optional.of(losingPlayer));
+
+        this.userService.tradeEloPoints(winningPlayer.getUsername(), losingPlayer.getUsername(), true);
+
+        Double result = winningPlayer.getEloRating();
+
+        // then
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).save(userArgumentCaptor.capture());
+
+        assertThat(winningPlayer)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("eloRating", result);
+
+    }
+
+    @Test
+    void shouldRemoveEloPointsFromLosingPlayer() {
+        User winningPlayer = initializeWinningPlayer();
+
+        User losingPlayer = initializeLosingPlayer();
+
+        // when
+        when(userRepository.findByUsername(winningPlayer.getUsername())).thenReturn(Optional.of(winningPlayer));
+        when(userRepository.findByUsername(losingPlayer.getUsername())).thenReturn(Optional.of(losingPlayer));
+
+        this.userService.tradeEloPoints(winningPlayer.getUsername(), losingPlayer.getUsername(), false);
+
+        Double result = losingPlayer.getEloRating();
+
+        // then
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+
+        verify(userRepository).save(userArgumentCaptor.capture());
+
+        assertThat(losingPlayer)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("eloRating", result);
+    }
+    private SignUpRequest initializeSignupRequest(){
+        SignUpRequest request = new SignUpRequest();
+        request.setEmail(email);
+        request.setUsername(username);
+        return request;
+    }
+
+    private User initializeWinningPlayer() {
         User winningPlayer = new User();
         String winningPlayerUsername = "Bolek";
         winningPlayer.setEloRating(1200.0);
         winningPlayer.setUsername(winningPlayerUsername);
+        return winningPlayer;
+    }
 
+    private User initializeLosingPlayer() {
         User losingPlayer = new User();
         String losingPlayerUsername = "Lolek";
         losingPlayer.setEloRating(1000.0);
         losingPlayer.setUsername(losingPlayerUsername);
-
-        // when
-        when(userRepository.findByUsername(winningPlayerUsername)).thenReturn(Optional.of(winningPlayer));
-        when(userRepository.findByUsername(losingPlayerUsername)).thenReturn(Optional.of(losingPlayer));
-
-        this.userService.tradeEloPoints(winningPlayerUsername, losingPlayerUsername, true);
-        this.userService.tradeEloPoints(winningPlayerUsername, losingPlayerUsername, false);
-
-        Double winningElo = winningPlayer.getEloRating();
-        Double losingElo = losingPlayer.getEloRating();
-
-        // then
-        assertThat(winningPlayer)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("eloRating", winningElo);
-
-        assertThat(losingPlayer)
-                .isNotNull()
-                .hasFieldOrPropertyWithValue("eloRating", losingElo);
+        return losingPlayer;
     }
 }
