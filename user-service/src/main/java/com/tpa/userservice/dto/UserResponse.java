c95c7e02537e3e0a14f23711b9bd9c7a0607010e
Tymoteusz Sztrumpf <tymoteusztrumpf@gmail.com>
2024-01-18 20:03:04 +0100

    change user-service: overrided equals() method in UserResponse class

--- user-service/src/main/java/com/tpa/userservice/dto/UserResponse.java
+++ user-service/src/main/java/com/tpa/userservice/dto/UserResponse.java
@@ -3,9 +3,19 @@ package com.tpa.userservice.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Builder
 @Getter
 public class UserResponse {
     String username;
     String email;
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) return true;
+        if(!(that instanceof UserResponse thatResponse)) return false;
+        return this.username.equals(thatResponse.username) && Objects.equals(this.email, thatResponse.email);
+    }
 }
+
